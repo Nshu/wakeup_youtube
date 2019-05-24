e7ee4458fbac55255ced20ef0d6eac90d1650d97
Nshu <kukkii09@gmail.com>
2019-05-25 06:06:57 +0900

    if W_DIR is empty, exit

--- drive_wakeup.sh
+++ drive_wakeup.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-W_DIR="${HOME}/wakeup_youtube"
+readonly W_DIR="${HOME}/wakeup_youtube"
+
+# if W_DIR is empty, exit
+if [[ -z "$(ls ${W_DIR})" ]]; then
+    exit 0
+fi
 
 bash ${W_DIR}/aplay_random.sh
 while [ $? -gt 0 ]
